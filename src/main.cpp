fe668969b668d62e5cc36d080e5f147a5585c988
Ilya Malgin <soundmaker1505@gmail.com>
2023-02-01 15:36:28 +0300

    KR-6 testApp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
+#include <wx/wx.h>
+#include <wx/app.h>
 
-using namespace std;
-
-int main()
+class TestFrame : public wxFrame
 {
-    cout << "Hello World!" << endl;
-    return 0;
-}
+public:
+    TestFrame(const wxString& title)
+        : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(1024, 768))
+    {
+        SetMinSize(wxSize(250, 200));
+    }
+};
+
+class TestApp : public wxApp
+{
+public:
+    virtual bool OnInit() override
+    {
+        if (!wxApp::OnInit())
+            return false;
+
+        TestFrame* frame = new TestFrame("Test");
+        frame->Show(true);
+
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(TestApp);
